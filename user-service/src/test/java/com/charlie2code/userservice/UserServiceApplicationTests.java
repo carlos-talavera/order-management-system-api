01d0d344626fc300d6a43b57d8f0b0f67fcce941
carlos-talavera <carloseduardorivera12@gmail.com>
2026-04-12 13:48:22 -0600

    test(user-service): set up application tests class

--- user-service/src/test/java/com/charlie2code/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/charlie2code/userservice/UserServiceApplicationTests.java
@@ -2,12 +2,21 @@ package com.charlie2code.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.ComposeContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.io.File;
+
+@Testcontainers
 @SpringBootTest
 class UserServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Container
+    public static ComposeContainer environment =
+            new ComposeContainer(new File("compose.yaml"))
+                    .withExposedService("postgres", 5432);
 
+    @Test
+    void contextLoads() {}
 }
