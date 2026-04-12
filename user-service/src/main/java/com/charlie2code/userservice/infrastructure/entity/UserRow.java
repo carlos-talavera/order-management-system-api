4b4a54c5a628ce997386d26a1109facab0cd2929
carlos-talavera <carloseduardorivera12@gmail.com>
2026-04-12 13:49:33 -0600

    feat(user-service, infrastructure): add user row entity

---
+++ user-service/src/main/java/com/charlie2code/userservice/infrastructure/entity/UserRow.java
@@ -0,0 +1,38 @@
+package com.charlie2code.userservice.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserRow {
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(name = "auth_id", nullable = false, unique = true)
+    private UUID authId;
+
+    @Column(name = "first_name", nullable = false, length = 40)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 40)
+    private String lastName;
+
+    @Column(name = "email", nullable = true, length = 256)
+    private String email;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+}
