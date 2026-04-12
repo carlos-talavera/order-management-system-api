30083097f129ae72c42a1d914e7efb0502a19acc
carlos-talavera <carloseduardorivera12@gmail.com>
2026-04-12 13:46:47 -0600

    feat(user-service, exceptions): add abstract domain exception class

---
+++ user-service/src/main/java/com/charlie2code/userservice/domain/exception/DomainException.java
@@ -0,0 +1,9 @@
+package com.charlie2code.userservice.domain.exception;
+
+public abstract class DomainException extends RuntimeException {
+    protected DomainException(String message) {
+        super(message);
+    }
+
+    protected DomainException(String message, Throwable cause) { super(message, cause ); }
+}
