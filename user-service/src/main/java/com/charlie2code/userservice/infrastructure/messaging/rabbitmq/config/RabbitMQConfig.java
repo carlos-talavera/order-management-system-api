bedd98125200ba9111424fe10eeca3182184b797
carlos-talavera <carloseduardorivera12@gmail.com>
2026-04-15 17:42:48 -0600

    feat(user-service, infrastructure): add RabbitMQ config
    Exchange and queue are for user events. First event is the creation of a user

---
+++ user-service/src/main/java/com/charlie2code/userservice/infrastructure/messaging/rabbitmq/config/RabbitMQConfig.java
@@ -0,0 +1,31 @@
+package com.charlie2code.userservice.infrastructure.messaging.rabbitmq.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String USER_EVENTS_EXCHANGE = "user.events";
+    public static final String USER_EVENTS_QUEUE = "user-service.user-events";
+    public static final String USER_CREATED_ROUTING_KEY = "user.created";
+
+    @Bean
+    public Queue userEventsQueue() {
+        return QueueBuilder.durable(USER_EVENTS_QUEUE).build();
+    }
+
+    @Bean
+    public TopicExchange userEventsExchange() {
+        return ExchangeBuilder.topicExchange(USER_EVENTS_EXCHANGE).durable(true).build();
+    }
+
+    @Bean
+    public Binding userCreatedBinding(Queue userEventsQueue, TopicExchange userEventsExchange) {
+        return BindingBuilder
+                .bind(userEventsQueue)
+                .to(userEventsExchange)
+                .with(USER_CREATED_ROUTING_KEY);
+    }
+}
