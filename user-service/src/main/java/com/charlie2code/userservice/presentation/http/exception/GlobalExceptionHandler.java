478e9e6395261141f171bceea27c0817366d29d4
carlos-talavera <carloseduardorivera12@gmail.com>
2026-04-17 13:01:52 -0600

    chore(user-service, presentation): include logging in global exception handler

--- user-service/src/main/java/com/charlie2code/userservice/presentation/http/exception/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/charlie2code/userservice/presentation/http/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.charlie2code.userservice.presentation.http.exception;
 
 import com.charlie2code.userservice.application.exception.ApplicationException;
 import com.charlie2code.userservice.domain.exception.DomainException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +15,8 @@ import java.time.Instant;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     private ResponseEntity<ProblemDetails> buildResponse(Exception ex, HttpStatus status, HttpServletRequest request) {
         ProblemDetails body = new ProblemDetails(
@@ -27,17 +31,19 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DomainException.class)
     public ResponseEntity<ProblemDetails> handleDomain(DomainException ex, HttpServletRequest request) {
+        log.warn("Domain error at [{}]: {}", request.getRequestURI(), ex.getMessage());
         return buildResponse(ex, HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<ProblemDetails> handleApplication(ApplicationException ex, HttpServletRequest request) {
+        log.warn("Application error at [{}]: {}", request.getRequestURI(), ex.getMessage());
         return buildResponse(ex, HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ProblemDetails> handleOther(Exception ex, HttpServletRequest request) {
-        ex.printStackTrace(); // TODO: use a logger
+        log.error("Unexpected error at [{}]", request.getRequestURI(), ex);
         return buildResponse(ex, HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 }
